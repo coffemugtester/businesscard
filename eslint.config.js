ae298a9e697b49a9f7c63d60ef6e6a4efd8b99a1
coffemugtester <coffeemugtester@gmail.com>
2025-11-12 21:40:22 +0100

    add playwright test

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": "off", // Turn off base rule
+      "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "off",
     },
   },
