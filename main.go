a789ca4df689c6cf3b0adb9f62cbf6f049730a30
coffemugtester <coffeemugtester@gmail.com>
2025-11-22 11:37:42 +0000

    add picture

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/hello.html"))
 		data := struct {
